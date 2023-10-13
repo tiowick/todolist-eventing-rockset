e6b0be0975a9f824121242f0ae2e07fc97bbc508
Jeferson sena <jefinho368@gmail.com>
2023-10-13 10:43:50 -0300

    Implemented UserContoroller.java

--- src/main/java/br/com/jefersonsena/todolist/user/UserController.java
+++ src/main/java/br/com/jefersonsena/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.jefersonsena.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,23 @@ public class UserController {
     //passando o objeto criado "UserModel"
     //dentro do body da request
 
-    @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
+    @Autowired
+    private IUserRepository userRepository;
 
-        System.out.println(userModel.getUserName());
+    @PostMapping("/")
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        
+        var user = this.userRepository.findByUserName(userModel.getUserName());
+
+        // se já existir
+        if(user != null) {
+            System.out.println("Usuário já existe");
+            // mensagem de erro 
+            // retornar status code caso ocorra erro
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe!");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
